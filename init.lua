4c22d39559d49e28d3f596ebdef57f4a5adb4322
w4daka <ignorenegativeideology999@gmail.com>
2026-01-21 10:26:32 +0900

    markdownlint add

--- init.lua
+++ init.lua
@@ -332,6 +332,20 @@ require('lazy').setup({
   --        end,
   --    }
   --
+
+  {
+    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+    -- used for completion, annotations and signatures of Neovim apis
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+      },
+    },
+  },
+
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`.
   --
--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -29,6 +29,27 @@ return {
         }
       end
     end,
+    formatters = {
+      -- You can use any formatter that conform.nvim supports
+      -- Find the list of supported formatters here:
+      ['markdown-toc'] = {
+        condition = function(_, ctx)
+          for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+            if line:find '<!%-%- toc %-%->' then
+              return true
+            end
+          end
+        end,
+      },
+      ['markdonlint-cli2'] = {
+        condition = function(_, ctx)
+          local diag = vim.tbl_filter(function(d)
+            return d.source == 'markdownlint'
+          end, vim.diagnostic.get(ctx.buf))
+          return #diag > 0
+        end,
+      },
+    },
     formatters_by_ft = {
       lua = { 'stylua' },
       -- Conform can also run multiple formatters sequentially
@@ -36,6 +57,9 @@ return {
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       javascript = { 'prettierd', 'prettier', stop_after_first = true },
+
+      ['markdown'] = { 'prettier', 'markdonlint-cli2', 'markdown-toc' },
+      ['markdown.mdx'] = { 'prettier', 'markdonlint-cli2', 'markdown-toc' },
     },
   },
 }
